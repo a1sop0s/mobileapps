5dceafc480f0af8d2a906423cf9937a074c4d781
Harri L <harri.liimann@gmail.com>
2021-03-12 16:28:27 +0200

    Added People.cs, RemoteDataService.cs

--- StarwarsApp/StarwarsApp/MainActivity.cs
+++ StarwarsApp/StarwarsApp/MainActivity.cs
@@ -1,19 +1,36 @@
-﻿using Android.App;
+﻿#nullable enable
+using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Content.PM;
+using StarwarsApp.Services;
 
 namespace StarwarsApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            
+            var remoteDataService = new RemoteDataService();
+            var data = await remoteDataService.GetStarwarsPeople();
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, 
+            string[]? permissions, Permission[]? grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, 
+                permissions, grantResults);
+            
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
---
+++ StarwarsApp/StarwarsApp/Services/RemoteDataService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace StarwarsApp.Services
+{
+    public class RemoteDataService
+    {
+        public async Task<People> GetStarwarsPeople()
+        {
+            var client = new HttpClient();
+            var response = await client
+                .GetStringAsync("https://swapi.dev/api/people/");
+            People data = null;
+
+            data = response != null
+                ? JsonConvert.DeserializeObject<People>(response)
+                : null;
+            return data;
+        }
+    }
+}
